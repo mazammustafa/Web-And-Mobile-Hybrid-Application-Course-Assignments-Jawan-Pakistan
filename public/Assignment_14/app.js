4eaaebdd62b390ec2aed7a3991c006f3f973786a
mazammustafa <bscs20-089@ssuet.edu.pk>
2024-02-22 16:23:40 -0800

    JP_ASSIGNMENT_14_DONE

---
+++ public/Assignment_14/app.js
@@ -0,0 +1,213 @@
+// var firstName = "Muhammmad";
+// var lastName = "Azam Mustafa";
+// var email = "azammustafa201@gmail.com";
+// var phone = "03116278526";
+// console.log("Welcome my name " + firstName 
+// + " " + lastName + " and my email address is " + email + " and i am using zong network and the number is " + phone);
+// alert("Welcome my name " + firstName 
+// + " " + lastName + " and my email address is " + email + " and i am using zong network and the number is " + phone);
+// document.write("Welcome my name " + firstName 
+// + " " + lastName + " and my email address is " + email + " and i am using zong network and the number is " + phone);
+
+// var number = 10;
+// number += 10;
+
+// console.log(number);
+
+
+
+
+
+// Chapter # 01
+
+// Q1 Write a script to greet your website visitor using JS alert box.
+
+alert("Chapter # 01");
+alert("Welcome To Our Side....");
+
+// 2. Write a script to display following message on your web page
+
+alert("Error! Please enter a valid password.");
+// 3. Write a script to display following message on your web
+// page: (Hint : Use line break)
+alert("Welcome to JS Land.. \n Happy Coding!")
+
+// 4. Write a script to display following messages in sequence:
+alert("Welcome to JS Land..");
+alert("Happy Coding!");
+
+// 5. Generate the following message through browser’s
+// developer console:
+
+alert("Hello... I can run JS through my web browser's console");
+
+// chapter # 02
+
+// 1. Declare a variable called username    
+alert("Chapter # 02");
+var username = "azammustafa";
+
+// 2. Declare a variable called myName & assign to it a string 
+// that represents your Full Name.
+var myName = "Muhammad Azam Mustafa";
+
+// 3. Write script to
+// a) Declare a JS variable, titled message.
+// b) Assign “Hello World” to variable message
+// c) Display the message in alert box.
+
+var message = "Hello World";
+alert(message);
+
+
+// 4. Write a script to save student’s bio data in JS variables and 
+// show the data in alert boxes.
+
+
+var firstName = "Azam Mustafa";
+var edu = "Certified WEB AND MOBILE HYBRID APPLICATION DEVELOPER";
+var age = "21 years old";
+
+alert(firstName);
+alert(edu);
+alert(age);
+
+// 5. Write a script to display the following alert using one JS 
+// variable
+
+var pizza = " PIZZA \n PIZZ \n PIZ \n PI \n P";
+
+alert(pizza);
+
+// 6. Declare a variable called email and assign to it a string that 
+// represents your Email Address(e.g. example@example.com). 
+// Show the blow mentioned message in an alert box.(Hint: use 
+// string concatenation)
+
+var email = "azammustafa201@gmail.com";
+alert("My email address is " + email);
+
+// 7. Declare a variable called book & give it the value “A
+// smarter way to learn JavaScript”. Display the following
+// message in an alert box:
+
+var Book = "A smarter way to learn JavaScript";
+alert("I am trying to learn from the book " + Book);
+
+// 8. Write a script to display this in browser through JS
+
+document.write("Yah! I can write HTML content through Javascript.");
+
+// 9. Store following string in a variable and show in alert and 
+// browser through JS
+var design = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+alert(design);
+
+// Chapter # 03
+
+// 1. Declare a variable called age & assign to it your age. Show
+// your age in an alert box.
+alert("Chapter # 03");
+var age = 21;
+alert("I am " + age + " year old ")
+
+// 2. Declare & initialize a variable to keep track of how many
+// times a visitor has visited a web page. Show his/her
+// number of visits on your web page. For example: “You
+// have visited this site N times”
+
+var N = 14;
+alert("You have visited this site" + N + " times");
+
+// 3. Declare a variable called birthYear & assign to it your
+// birth year. Show the following message in your browser:
+
+var birthYear = 2002;
+document.write("My Birth Year is " + birthYear + "<br /> Data type of my declared variable is " + typeof birthYear + "<br />");
+
+// 4. A visitor visits an online clothing store
+// www.xyzClothing.com . Write a script to store in variables
+// the following information:
+// a. Visitor’s name
+// b. Product title
+// c. Quantity i.e. how many products a visitor wants to
+// order
+// Show the following message in your browser: “John
+// Doe ordered 5 T-shirt(s) on XYZ Clothing store”
+
+var visitorsName = "John Doe";
+var productTitle = "XYZ Clothing store";
+var quantity = "5 T-shirt(s)";
+
+document.write("<b>" + visitorsName + "</b> ordered <b>" + quantity + "</b>" + productTitle);
+
+alert("Chapter # 01-03");
+// CHAPTER #1
+
+// Q1
+// I. First Name
+alert("First Name: ");
+
+// II. Last Name
+alert("Last Name: ");
+
+// III. Email
+alert("Email: ");
+
+// IV. Phone Number
+alert("Phone Number: ");
+
+// V. Password
+alert("Password: ");
+
+
+// Q2
+alert("The Correct statement is \nalert(You're learning JavaScript!");
+
+// Q3
+alert("THIS IS JS ASSIGNEMENT NO. 01");
+
+
+// CHAPTER # 2
+// 1. Declare any variable in the camel Case format.
+var myVariableName;
+
+// 2. Declare a variable of your choice without defining it. Then, in a second statement, assign it a string of your choice.
+myVariable = "Hello world!";
+
+// 3. Declare the variable teamName and Alert your Team name.
+
+var teamName = "Champions";
+alert("Our team name is: " + teamName);
+
+// 4. This statement has already been coded. var bestMan = &quot;Charlie&quot;; Assign the variable a new string.
+
+var bestMan = "AZAM";
+bestMan = "MUHAMMAD AZAM MUSTAFA";
+
+// CHAPTER # 3
+
+// 1. Declare a variable “caseQty”
+var caseQty;
+
+// Assign to the variable caseQty, which has already been declared, the value 144.
+var caseQty;
+caseQty = 144;
+
+// 3. Rewrite this statement so the variable can be used in a math operation. var num = &quot;9&quot;;
+var num = 9;
+
+// 4. In one statement declare a variable. In a second statement assign it the sum of 2 numbers.
+var sum;
+sum = 5 + 3;
+
+// What is the value of orderTotal? var merchTotal = 100;var shippingCharge = 10;var orderTotal = merchTotal + shippingCharge; Tryit yourself.
+var merchTotal = 100;
+var shippingCharge = 10;
+var orderTotal = merchTotal + shippingCharge;
+alert("Order Total: " + orderTotal);
+
+// In the first statement declare a variable and assign it a number. In the second statement, change the value of the variable by adding it together with a number.
+var number = 5; 
+number += 3; 
+
